c48b3a8abe8d5650890efe49be0c21f13f53c745
alex <aastrotenko@mail.ru>
2024-05-30 21:39:56 +0700

    Adding dist

--- src/features/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as iPlayer } from './iPlayer.vue'
-export { default as iSidebar } from './iSidebar.vue'
-export { default as iRadioList } from './iRadioList.vue'
-export { default as iLovePwa } from './iLovePwa.vue'
